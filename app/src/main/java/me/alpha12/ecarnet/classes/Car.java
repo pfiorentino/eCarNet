4553c5494a8946584aebd13c8f4c986192180f50
Paul FIORENTINO <fiorentino.paulalain@gmail.com>
2015-10-26 12:15:07 +0100

    Fix fucking bug qui n'en était pas un

---
+++ app/src/main/java/me/alpha12/ecarnet/classes/Car.java
@@ -0,0 +1,14 @@
+package me.alpha12.ecarnet.classes;
+
+/**
+ * Created by paul on 02/10/2015.
+ */
+public class Car {
+    public String plateNum;
+    public Model model;
+
+    public Car(String plateNum, Model model) {
+        this.plateNum = plateNum;
+        this.model = model;
+    }
+}
---
+++ app/src/main/java/me/alpha12/ecarnet/classes/Model.java
@@ -0,0 +1,16 @@
+package me.alpha12.ecarnet.classes;
+
+/**
+ * Created by paul on 15/10/2015.
+ */
+public class Model {
+    public String brand;
+    public String model;
+    public String engine;
+
+    public Model(String brand, String model, String engine) {
+        this.brand = brand;
+        this.model = model;
+        this.engine = engine;
+    }
+}
