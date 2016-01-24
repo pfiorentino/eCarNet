6407dcd7f1d9fa4c6676821aa7b6f304a023deb8
Paul FIORENTINO <fiorentino.paulalain@gmail.com>
2016-01-24 22:05:31 +0100

    Empty message (alias PleinG de modifes)

--- app/src/main/java/me/alpha12/ecarnet/adapters/NFCTagTypeAdapter.java
+++ app/src/main/java/me/alpha12/ecarnet/adapters/NFCTagTypeAdapter.java
@@ -49,9 +49,11 @@ public class NFCTagTypeAdapter extends ArrayAdapter<NFCTagType> {
         if (objects.get(position).isSelected()) {
             holder.checkmark.setVisibility(View.VISIBLE);
             holder.imageView.setBackgroundResource(R.drawable.selected_tag_type_border);
+            holder.textView.setBackgroundColor(0x88F44336);
         } else {
             holder.checkmark.setVisibility(View.GONE);
             holder.imageView.setBackgroundColor(0x88FFFFFF);
+            holder.textView.setBackgroundColor(0x1F000000);
         }
 
         return convertView;
